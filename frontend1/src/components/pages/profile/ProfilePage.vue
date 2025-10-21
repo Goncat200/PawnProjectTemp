f78183022d73cb64fe9131cd7ce39c10d3b82685
Goncat200 <146470859+Goncat200@users.noreply.github.com>
2025-10-21 12:28:47 +0500

    Delete frontend1 directory

--- frontend1/src/components/pages/profile/ProfilePage.vue
+++
@@ -1,9 +0,0 @@
-<script setup> 
-</script>
-
-<template>
-    <div class="Prifile">
-        <h1>Welcome to the Profile Page</h1>
-        <p>This is the main content of the Profile page.</p>
-    </div>
-</template>
--- frontend1/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp, ref } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-  createApp(App).mount('#app')
-
-// export const globalState = {
-//   isReg: ref(false)
-// }
